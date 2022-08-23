a3948d3344b06a535ff9bd5943959f87b14ca596
ardika24 <ardhieka24@gmail.com>
2022-08-23 21:28:47 +0700

    add production config and Procfile

--- config/config.js
+++ config/config.js
@@ -16,10 +16,16 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    host: "localhost",
+    // Reference: https://stackoverflow.com/a/70243144
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
